5301212a988c4992d974535eaa3e856c8ed44869
Trevor McReynolds <trevor@mcxreynolds.dev>
2025-01-18 20:01:10 -0800

    update UI/Tailwind markup for blog app pages

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,11 +6,8 @@ app_name = "core"
 urlpatterns = [
     path("", views.home_page_view, name="index"),
     path("contact", views.contact_page_view, name="contact"),
-    path("now", views.now_page_view, name="now"),
-    path("social", views.social_page_view, name="social"),
+    path("about", views.about_page_view, name="about"),
     path("work", views.work_page_view, name="work"),
     path("projects", views.project_list_view, name="projects"),
     path("snippets", views.snippet_list_view, name="snippets"),
-    path("blog", views.blog_list_view, name="blog"),
 ]
-
--- apps/core/views.py
+++ apps/core/views.py
@@ -7,11 +7,8 @@ def home_page_view(request):
 def contact_page_view(request):
     return render(request, "pages/contact.html")
 
-def now_page_view(request):
-    return render(request, "pages/now.html")
-
-def social_page_view(request):
-    return render(request, "pages/social.html")
+def about_page_view(request):
+    return render(request, "pages/about.html")
 
 def work_page_view(request):
     return render(request, "pages/work.html")
@@ -21,6 +18,3 @@ def project_list_view(request):
 
 def snippet_list_view(request):
     return render(request, "pages/snippet_list.html")
-
-def blog_list_view(request):
-    return render(request, "pages/blog_list.html")
